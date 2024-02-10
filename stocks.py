9da8ec72aadb1c617380464383b34e4792d0cc69
zivcohen1 <146225919+zivcohen1@users.noreply.github.com>
2024-02-10 15:04:00 -0500

    Add files via upload

--- stocks.py
+++ stocks.py
@@ -1,19 +1,22 @@
-#stocks transaction program
-commission=3/100
-shares_purchased=float(input("please type in the number of shares you purchased: "))
-purchase_price=float(input("please type in the price per share purchased: "))
-shares_sold=float(input("please type in the number of shares you sold: "))
-sold_price=float(input("please type in the price per share sold: "))
+#CS175
+#Ziv Cohen
+#stocks
+commission_purchase=float(input("Please enter commision rate percentage (ex 0.03) on stock purchase: "))
+commission_sale=float(input("Please enter commission rate percentage (ex 0.03) on stock sale: "))
+shares_purchased=float(input("please enter number of shares you purchased: "))
+shares_sold=float(input("please enter number of shares you sold: "))
+purchase_price=float(input("Please enter purchase price per share : "))
+sold_price=float(input("please enter sell price per share: "))
 amount_paid= shares_purchased * purchase_price
-print("Amount paid for the stock: ", amount_paid)
-commission_paid=commission * amount_paid
-print("Commission paid on the purchase: ", commission_paid)
+print("Amount paid for the stock: ", f'${amount_paid:,.2f}')
+commission_paid= commission_purchase * amount_paid
+print("Commission paid on the purchase: ", f'${commission_paid:,.2f}')
 amount_sold= shares_sold * sold_price
-print("Amount the stock sold for: ", amount_sold)
-commission_sold=commission * amount_sold
-print("commission paid on the sale: ", commission_sold)
-profit=amount_sold - amount_paid
+print("Amount the stock sold for: ", f'${amount_sold:,.2f}')
+commission_sold= commission_sale * amount_sold
+print("commission paid on the sale: ", f'${commission_sold:,.2f}')
+profit= amount_sold - (amount_paid + commission_sold + commission_paid)
 if profit>0:
-    print("Congrats! you made profit. this is your profit: ", profit)
+    print("Congrats! you made profit. this is your profit: ", f'${profit:,.2f}')
 else:
-    print("You lost money, better luck next time! this is your loss: " , profit )
+    print("You lost money, better luck next time! this is your loss: " , f'${profit:,.2f}' )
